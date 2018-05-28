f45ca0659ec31c9553fa55ee6f93f0fce7cfc70d
Mauricio Rojas <Mauricio.Rojas@fundacion-jala.org>
2018-05-27 22:07:12 -0400

    mapa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MapaComponent } from './componentes/mapa/mapa.component';
 
-
+import { AgmCoreModule } from '@agm/core';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD0O6RTnNKcK0iKfgQxe379q8aqW82IBmo'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/mapa/mapa.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Coordinates } from '../../modelos/mock-coordinates';
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.component.html',
+  styleUrls: ['./mapa.component.css']
+})
+export class MapaComponent implements OnInit {
+
+  eventos = Coordinates;
+  titulo = 'Mapa de eventos';
+  lat = -17.391848;
+  lng = -66.256003;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
