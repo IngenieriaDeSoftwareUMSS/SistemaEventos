d6253f25601dd2b048533ee0e81695699667da89
MauricioCadima <SrSilpancho@gmail.com>
2018-06-28 00:09:42 -0400

    componente mapa actualizado

--- src/app/componentes/mapa/mapa.component.ts
+++ src/app/componentes/mapa/mapa.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Coordinates } from '../../modelos/mock-coordinates';
 import { Evento } from '../../modelos/evento';
 import { single } from 'rxjs/operators';
+import {MarcadorService} from 'marcador.service.ts';
 
 @Component({
   selector: 'app-mapa',
@@ -15,10 +16,18 @@ export class MapaComponent implements OnInit {
   lat = -17.391848;
   lng = -66.256003;
   eventoActual = new Evento();
-  constructor() { }
+  constructor(private marcadorSrv :MarcadorService ) { }
 
   ngOnInit() {
+    this.getMarcadores();
   }
+  getMarcadores()
+  {
+    this.marcadorSrv.getEventos().subscribe(
+      data => { this.eventos = data; },
+      err => console.error(err),
+      () => console.log('exito cargando eventos')
+  };
   modalEvento(evento: Evento) {
     this.eventoActual = evento;
     document.getElementById("openModalButton").click();
